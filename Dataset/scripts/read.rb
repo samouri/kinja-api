98f757f32db9a9ed2ec8eb5898ebcd76278dc567
Ali Eskandari <alieskandari3@gmail.com>
2015-03-24 12:02:49 -0400

    Added read.rb to read json file into article objs
    Read script reads from output.json and creates article objects

---
+++ Dataset/scripts/read.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+require 'ostruct'
+require 'json'
+require 'pp'
+
+filename = "../output/output.json"
+articles = []
+
+File.open(filename, "r:UTF-8") { |f|
+	articles = f.readlines.map { |e| OpenStruct.new(JSON.parse(e.chomp!)) }
+}
+
+pp articles
+
