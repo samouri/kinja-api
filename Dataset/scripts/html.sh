d0bb8cc9060a3dd8672b693bc6dcb620c7e3d7fc
Jake Fried <jzfisgreat@gmail.com>
2015-03-25 10:35:21 -0400

    relatively fast and working versions of viewcounts.sh (which outputs a json of id, unique, nonUnique.  also of htmls.sh which downloads the html (preAJAX calls)

---
+++ Dataset/scripts/html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+html() { 
+  url="$1";
+  regex="[^0-9a-z.-]"
+  filename="$(echo $url | sed s/"$regex"//g)"
+
+   curl --silent "${url}" > "../outputs/htmlh/${filename}"
+}
+export -f html
+
+cat ../input/filtered_gawker_urls.txt | xargs -I {} -P 20 -n 1 -s 9999999999  bash -c "html {}"
+
--- Dataset/scripts/viewcounts.sh
+++ Dataset/scripts/viewcounts.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-#sitemaps= curl http://gawker.com/sitemap.xml | grep -oP "http:(.*?)endTime.{20}"
-regex="([0-9]{5,20})"
-
-echo "{"
-while read url
-do 
- (
-  [[ $url =~ $regex ]]
-  id="${BASH_REMATCH[1]}"
-
+json() { 
+  regex="([0-9]{5,20})"
+  url="$1";
+  [[ $url =~ $regex ]];
+  id="${BASH_REMATCH[1]}";
   viewcount="$(curl --silent "http://kinja.com/api/analytics/stats/post?id=$id")"
   unique="$( echo "${viewcount}" | grep -Po '"unique":.*?[^\\],' | sed 's/[^0-9]*//g')"
   nonUnique="$( echo "${viewcount}" | grep -Po '"nonUnique":.*?[^\\],' | sed 's/[^0-9]*//g')"
   
-  echo "'$id': {'unique': $unique, 'nonUnique': $nonUnique}," )
-done < "${1:-/dev/stdin}"
+  echo "'$id': {'unique': $unique, 'nonUnique': $nonUnique},";
+}
+export -f json
 
-echo "'-1': {'finished': true }}"
+
+echo "{"
+
+cat ../input/filtered_gawker_urls.txt | xargs -I {} -P 20 -n 1 -s 9999999999  bash -c "json {}"
+
+echo "'-1': {'finished': true }"
+echo "}"
+
